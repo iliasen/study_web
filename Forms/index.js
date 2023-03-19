c60373bb6d5a761163d532342235cffc5cf5fe6a
iliasen <113366047+iliasen@users.noreply.github.com>
2023-03-20 00:09:41 +0300

    Compete first page & add clear textarea

--- Forms/index.js
+++ Forms/index.js
@@ -4,13 +4,6 @@ function clearAllFormInputs() {
     for (let input of inputs) {
         input.value = "";
     }
+    const textarea = document.getElementById("ta");
+    textarea.value = "";
 }
-
-
-// const form = document.getElementById("form");
-//     form.addEventListener("submit",function (event){
-//         event.preventDefault()
-//         const formValues = new FormData(event.currentTarget)
-//         localStorage.setItem("formValues", JSON.stringify(event.currentTarget))
-//         console.log(event.currentTarget)
-//     })
